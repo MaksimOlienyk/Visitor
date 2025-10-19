1924820b3cd1f94ba822f052b5e385f9770b631c
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 19:15:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿interface IVisitor { void Visit(ElementA a); }
+interface IElement { void Accept(IVisitor v); }
+
+class ElementA : IElement { public void Accept(IVisitor v) => v.Visit(this); }
+
+class ConcreteVisitor : IVisitor { public void Visit(ElementA a) => Console.WriteLine("Visited A"); }
+
+class Program
+{
+    static void Main()
+    {
+        IElement e = new ElementA();
+        e.Accept(new ConcreteVisitor());
+    }
+}
+
